200e2a5152836a85894bfa149a5b38f343d3d0b1
Oryxel <172038521+Oryxel@users.noreply.github.com>
2025-02-07 22:21:12 +0700

    Cancel attack if the entity does not exist on client side.

--- src/main/java/ac/boar/anticheat/check/impl/hitbox/HitBoxA.java
+++ src/main/java/ac/boar/anticheat/check/impl/hitbox/HitBoxA.java
@@ -3,7 +3,6 @@ package ac.boar.anticheat.check.impl.hitbox;
 import ac.boar.anticheat.check.api.impl.PacketCheck;
 import ac.boar.anticheat.compensated.cache.BoarEntity;
 import ac.boar.anticheat.player.BoarPlayer;
-import ac.boar.anticheat.util.math.Vec3f;
 import ac.boar.protocol.event.CloudburstPacketEvent;
 import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryTransactionType;
 import org.cloudburstmc.protocol.bedrock.packet.InteractPacket;
@@ -32,7 +31,8 @@ public class HitBoxA extends PacketCheck {
 
         final BoarEntity entity = player.compensatedWorld.getEntity(packet.getRuntimeEntityId());
         // Some simple validation.
-        final boolean tooFarOrInvalid = entity == null || entity.getServerPosition().distance(player.x, player.y, player.z) > 6.0 || entity.getPosition().distance(player.x, player.y, player.z) > 6.0;
+        final boolean tooFarOrInvalid = entity == null || entity.getTransactionId() > player.lastReceivedId ||
+                entity.getServerPosition().distance(player.x, player.y, player.z) > 6.0 || entity.getPosition().distance(player.x, player.y, player.z) > 6.0;
         if (tooFarOrInvalid) {
             event.setCancelled(true);
             return;
