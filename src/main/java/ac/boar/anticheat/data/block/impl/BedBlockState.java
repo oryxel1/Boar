9d97354492ef83359fa96c27fec9ce8a9a58e8bd
oryxel1 <gamingnoob681@gmail.com>
2025-08-22 20:00:48 +0700

    Fixed bed y velocity tick end.

--- src/main/java/ac/boar/anticheat/data/block/impl/BedBlockState.java
+++ src/main/java/ac/boar/anticheat/data/block/impl/BedBlockState.java
@@ -2,12 +2,9 @@ package ac.boar.anticheat.data.block.impl;
 
 import ac.boar.anticheat.data.block.BoarBlockState;
 import ac.boar.anticheat.player.BoarPlayer;
-import ac.boar.anticheat.util.math.Box;
-import ac.boar.anticheat.util.math.Vec3;
 import org.cloudburstmc.math.vector.Vector3i;
+import org.cloudburstmc.protocol.bedrock.data.entity.EntityFlag;
 import org.geysermc.geyser.level.block.type.BlockState;
-
-import java.util.List;
 
 public class BedBlockState extends BoarBlockState {
     public BedBlockState(BlockState state, Vector3i position, int layer) {
@@ -16,34 +13,14 @@ public class BedBlockState extends BoarBlockState {
 
     @Override
     public void updateEntityMovementAfterFallOn(BoarPlayer player, boolean living) {
-        // Don't ask questions, just know that it does works.
-        List<Box> boxes = this.findCollision(player, getPosition(), null, false);
-        float maxY = 0;
-        for (Box box : boxes) {
-            maxY = Math.max(maxY, box.maxY);
-        }
-
-        if (player.position.y - maxY > 0.5) {
-            player.velocity.y = 0;
-            return;
-        }
-
-        final float d = living ? 1.0F : 0.8F;
-        Vec3 velocity = player.velocity.clone();
-        velocity.y = -velocity.y * 0.75F * d;
-
-        if (velocity.y > 0.75) {
-            velocity.y = 0.75F;
-        }
-
-        if (player.velocity.y < 0) {
-            if (player.unvalidatedTickEnd.y > 0) {
-                player.velocity = velocity;
-            } else {
-                player.velocity.y = 0;
+        if (player.velocity.y < 0 && !player.getFlagTracker().has(EntityFlag.SNEAKING)) {
+            final float d = living ? 1.0F : 0.8F;
+            player.velocity.y = -player.velocity.y * 0.75F * d;
+            if (player.velocity.y > 0.75) {
+                player.velocity.y = 0.75F;
             }
         } else {
-            player.velocity = velocity;
+            player.velocity.y = 0;
         }
     }
 }
