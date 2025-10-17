705895f718e319ffd41f62e1ed1f4c2df0330f7a
oryxel <gamingnoob681@gmail.com>
2025-10-17 19:08:58 +0700

    Added workaround for inventory desync. (#40)
    * Workaround for the inventory issue.
    
    * Don't pass latency to the server.
    
    * Start with id 1.

--- src/main/java/ac/boar/anticheat/packets/other/NetworkLatencyPackets.java
+++ src/main/java/ac/boar/anticheat/packets/other/NetworkLatencyPackets.java
@@ -4,6 +4,7 @@ import ac.boar.geyser.util.GeyserUtil;
 import ac.boar.protocol.event.CloudburstPacketEvent;
 import ac.boar.protocol.listener.PacketListener;
 import org.cloudburstmc.protocol.bedrock.packet.NetworkStackLatencyPacket;
+import org.geysermc.geyser.session.GeyserSession;
 
 public class NetworkLatencyPackets implements PacketListener {
     public final static long LATENCY_MAGNITUDE = 1000000L;
@@ -18,14 +19,26 @@ public class NetworkLatencyPackets implements PacketListener {
 
         // Positive id is for keep alive passthrough hack, and there also only 2 other negative id that we just need to check for.
         // This implementation could be a problem later on considering that Networking API will soon become a thing but oh welp.
-        if (id >= 0 || id == GeyserUtil.MAGIC_FORM_IMAGE_HACK_TIMESTAMP || id == GeyserUtil.MAGIC_VIRTUAL_INVENTORY_HACK) {
+        // Let's hope there isn't anything else that will conflict with our anticheat latency system.
+        if (id >= 0 || id == GeyserUtil.MAGIC_FORM_IMAGE_HACK_TIMESTAMP) {
+            return;
+        }
+
+        // There is this weird bug with virtual inventory hack, so uhhh, this is the work around for it
+        // TODO: Properly fix Boar to actually resolve this (https://github.com/oryxel1/Boar/issues/29).
+        if (id == GeyserUtil.MAGIC_VIRTUAL_INVENTORY_HACK) {
+            final GeyserSession session = event.getPlayer().getSession();
+            if (session.getPendingOrCurrentBedrockInventoryId() == -1) { // There is no hack to be done here.
+                return;
+            }
+
+            if (session.getInventoryHolder() != null) {
+                session.getInventoryHolder().pending(true); // Yep.
+            }
             return;
         }
 
         event.getPlayer().getLatencyUtil().confirmStackId(Math.abs(id));
-
-        // No reason to pass these to Geyser since it won't use it anyway... If Geyser suddenly decide to send a new id for a new hack
-        // then manually hardcoded it here... It's easier to handle thing that way.
         event.setCancelled(true);
     }
 }
