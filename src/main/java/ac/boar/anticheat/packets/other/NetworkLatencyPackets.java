bca5885b7e098db3fd7a22d874e232335c6c00c3
Alessandro <aleytnoos12@gmail.com>
2025-10-24 14:28:30 +0200

    Add PlayStation specific latency magnitude handling for NetworkLatency packets (#44)
    * Add PS4-specific latency magnitude handling for NetworkLatency packets
    
    * Added comments, also rename PS4 -> PS5.
    
    ---------
    
    Co-authored-by: oryxel <gamingnoob681@gmail.com>

--- src/main/java/ac/boar/anticheat/packets/other/NetworkLatencyPackets.java
+++ src/main/java/ac/boar/anticheat/packets/other/NetworkLatencyPackets.java
@@ -4,10 +4,12 @@ import ac.boar.geyser.util.GeyserUtil;
 import ac.boar.protocol.event.CloudburstPacketEvent;
 import ac.boar.protocol.listener.PacketListener;
 import org.cloudburstmc.protocol.bedrock.packet.NetworkStackLatencyPacket;
+import org.geysermc.api.util.BedrockPlatform;
 import org.geysermc.geyser.session.GeyserSession;
 
 public class NetworkLatencyPackets implements PacketListener {
     public final static long LATENCY_MAGNITUDE = 1000000L;
+    public final static long PS5_LATENCY_MAGNITUDE = 1000L;
 
     @Override
     public void onPacketReceived(final CloudburstPacketEvent event) {
@@ -15,7 +17,10 @@ public class NetworkLatencyPackets implements PacketListener {
             return;
         }
 
-        long id = packet.getTimestamp() / LATENCY_MAGNITUDE;
+        final GeyserSession session = event.getPlayer().getSession();
+        // Bedrock player have different latency magnitude depending on the platform, however this is the only one we know about
+        // TODO: Figure out what the magnitude for PS4 is, currently we only know PS5 (BedrockPlatform.PS4 is an misleading name.)
+        long id = packet.getTimestamp() / (session.platform() == BedrockPlatform.PS4 ? PS5_LATENCY_MAGNITUDE : LATENCY_MAGNITUDE);
 
         // Positive id is for keep alive passthrough hack, and there also only 2 other negative id that we just need to check for.
         // This implementation could be a problem later on considering that Networking API will soon become a thing but oh welp.
@@ -27,7 +32,6 @@ public class NetworkLatencyPackets implements PacketListener {
         // There is this weird bug with virtual inventory hack, so uhhh, this is the work around for it
         // TODO: Properly fix Boar to actually resolve this (https://github.com/oryxel1/Boar/issues/29).
         if (id == GeyserUtil.MAGIC_VIRTUAL_INVENTORY_HACK) {
-            final GeyserSession session = event.getPlayer().getSession();
             if (session.getPendingOrCurrentBedrockInventoryId() == -1) { // There is no hack to be done here.
                 return;
             }
