a17ac43d917d82d1723f2302813d29301bad7ebe
Rayhan S <rayhandes7@gmail.com>
2026-08-23 23:19:40 +0700

    inf effect fix

--- common/src/main/java/ac/boar/anticheat/ack/BoarDefaultAcknowledgments.java
+++ common/src/main/java/ac/boar/anticheat/ack/BoarDefaultAcknowledgments.java
@@ -347,7 +347,9 @@ public final class BoarDefaultAcknowledgments {
             if (ack.effect().equals(Effect.SPEED) || ack.effect().equals(Effect.SLOWNESS)) {
                 player.clientNeedsMovementSpeedAttributeUpdate = true;
             }
-            player.getActiveEffects().put(ack.effect(), new StatusEffect(ack.effect(), ack.amplifier(), ack.duration() + 1));
+            final int raw = ack.duration();
+            final int duration = raw < 0 || raw == Integer.MAX_VALUE ? Integer.MAX_VALUE : raw + 1;
+            player.getActiveEffects().put(ack.effect(), new StatusEffect(ack.effect(), ack.amplifier(), duration));
         } else if (ack.event() == MobEffectPacket.Event.REMOVE) {
             player.getActiveEffects().remove(ack.effect());
         }
